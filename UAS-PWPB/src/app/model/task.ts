0933aff800871673cec58ca1d09a8fb1aa0ba9a2
rezafp01 <noblesvan@gmail.com>
2020-07-05 15:54:14 +0700

    UAS Individu PWPB

---
+++ UAS-PWPB/src/app/model/task.ts
@@ -0,0 +1,5 @@
+export interface Task {
+    id?:string;
+    nama?:string;
+    daily?:string
+}
---
+++ UAS-PWPB/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyD9KToHOOIC2b0ALVPNYX2FG-mMJixS69o",
+    authDomain: "uaspwpb-9445c.firebaseapp.com",
+    databaseURL: "https://uaspwpb-9445c.firebaseio.com",
+    projectId: "uaspwpb-9445c",
+    storageBucket: "uaspwpb-9445c.appspot.com",
+    messagingSenderId: "888340344499",
+    appId: "1:888340344499:web:91103d31b023540a5e4ac6"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
